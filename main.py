885d8022a13ef13aa24f060edd495054d79582d5
Mirbek Nurjauov <mirbekkazax@gmail.com>
2024-09-14 20:23:17 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,96 @@
+# all_products = {'Склад': {'name': 'Хлеб', 'количество': 34}}
+#
+# def get_products(a = 'name'):
+#     print(all_products['Склад'][a])
+#
+# get_products()
+
+
+# def spammer(*args):
+#     for a in args:
+#         print(a)
+#
+# spammer(1, 2, 3 , 4, 5, 6, 7, 8, 9, 'GGAGAA', '2131231r')
+
+
+# def spam1(**kwargs):
+#     for k,v in kwargs.items():
+#         print(k,v)
+#
+# print(spam1(name= 'my1', age=23))
+
+
+# class User:
+#     name = 'Jacob'
+#     name2 = 'Jame'
+#     name3 = 'Rick'
+
+
+# class Car:
+#     def __init__(self, model, year, color):
+#         self.model = model
+#         self.year = year
+#         self.color = color
+#
+#
+#
+#
+# class Comment:
+#     def __init__(self, username, text, likes=0):
+#         self.username = username
+#         self.text = text
+#         self.likes = lik
+# class Car:
+#     def __init__(self, model, year, color):
+#         self.model = model
+#         self.year = year
+#         self.color = color
+#
+# class Cr:
+#
+#
+#    def stop(self):
+#        print('Машина остановилась')
+#
+# chevrolet = Car(model= 'Jentra', color='Black', year=2021)
+# chevrolet.stop()
+
+#
+# class Bankaccount:
+#     def __init__(self, name, balance=0):
+#         self.balance = balance
+#         self.name = name
+#
+#     def deposit(self, cash_amount):
+#         self.balance += cash_amount
+#
+#
+#     def cash(self, cash_amount):
+#         self.cash_amount -= cash_amount
+#
+#     def my_balance(self):
+#         return self.balance
+#
+# sasha = Bankaccount('Саша')
+# print(sasha.my_balance())
+# sasha.deposit(1)
+# print(sasha.my_balance())
+
+#
+# class User:
+#     def __init__(self, name, mail, age, number):
+#         self.name = name
+#         self.mail = mail
+#         self.age = age
+#         self.number = number
+#     def change_username(self, username):
+#         self.name = username
+#     def change_number(self, number1):
+#         self.number = number1
+#     def change_mail(self, mail1):
+#         self.mail = mail1
+#
+# pasha = User('Pavel', 'pasdasd', 23, '+998999199')
+# print(pasha.name)
+# print(name_chang)
+# name_change = input('Введите новое имя: ')
